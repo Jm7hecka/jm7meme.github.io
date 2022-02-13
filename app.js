2cd8a79e8006bbf0cf4f7ca99dc138851da8457c
jm7meme <77819603+jm7meme@users.noreply.github.com>
2022-02-13 17:06:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 function menu(x) {
-    document.getElementById('menu').style.width = '20%';
-    document.getElementById('menu2').style.width = '20%';
-    x.style.opacity = '0';
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        document.getElementById('menu').style.width = '100%';
+        document.getElementById('menu2').style.width = '100%';
+        x.style.opacity = '0';
+    }else{
+        document.getElementById('menu').style.width = '20%';
+        document.getElementById('menu2').style.width = '20%';
+        x.style.opacity = '0';
+    }
+
 }
 function closemenu() {
     document.getElementById('menu').style.width = '0%';
